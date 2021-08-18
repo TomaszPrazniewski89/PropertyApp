f862e0d5e65aeafd85fcb6d5b06c879d7bd849f6
Tomasz Prazniewski <tomaszprazniewski89@gmail.com>
2021-08-18 22:29:15 +0200

    update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,20 @@
 class CategoriesController <ApplicationController
   def index
     # w consoli Category.where(category: nil).to_sql
+    # @current_category = Category.where(category_id: params[:id])
+    @current_category = Category.find_by(id: params[:id])
+
+
+    @subcategories = subcategories.all
     binding.pry
-    @current_category = Category.where(category_id: params[:id])
 
+  end
 
+  def subcategories
+      # szuka po relacji w bazie model>category has_many :subcategories,
+      binding.pry
+
+    @current_category&.subcategories || Category.where(category_id: nil)
   end
 
 end
