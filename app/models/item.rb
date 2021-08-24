18c09dbfc8479a4c96e66861047db3e44459136a
Tomasz Prazniewski <tomaszprazniewski89@gmail.com>
2021-08-24 20:30:47 +0200

    add validations to model: item

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,18 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :type
   mount_uploader :image, ImageUploader
+
+
+
+  validates :title, presence: true
+  validates :cena, presence: true
+  validates :metraz, presence: true
+  validates :poziom, presence: true
+  validates :telefon, presence: true
+  validates :telefon, presence: true
+  validates :opis, presence: true
+  validates :pokoje, presence: true
+  validates :type_id, presence: true
+  validates :image, presence: true
+  validates :category_id, presence: true
 end
